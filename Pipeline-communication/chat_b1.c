93121b0bbf93a92cd9429e5964ffbef3e42fb98f
sky <644076264@qq.com>
2019-06-25 12:40:41 +0800

    Through four Windows

---
+++ Pipeline-communication/chat_b1.c
@@ -0,0 +1,14 @@
+#include"func.h"
+int main()
+{
+	int flag=0;
+	int msg_id=msgget(1000,0600|IPC_CREAT);
+	struct msgbuf msg;
+	while(1)
+	{
+		bzero(&msg,sizeof(msg));
+		msgrcv(msg_id,&msg,sizeof(msg.mtext),1,0);
+		printf("%s\n",msg.mtext);
+	}
+	return 0;
+}
